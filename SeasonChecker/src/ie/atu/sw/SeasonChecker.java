8d55ad7834c240d6af91810ac2d08a3bfcd1eaa8
siog <siog@LAPTOP-545TJ6PE>
2025-03-20 13:07:40 +0000

    Create switch expression to return season based on month
    Add case statements only; default
    statement will be added in next commit

--- SeasonChecker/src/ie/atu/sw/SeasonChecker.java
+++ SeasonChecker/src/ie/atu/sw/SeasonChecker.java
@@ -5,7 +5,16 @@ import java.time.*; //or import java.time.LocalDate;
 public class SeasonChecker {
 	
 	public String getSeason(LocalDate date) {
-		return null;
+		Month month = date.getMonth();
+		
+		//Switch expression, not a switch statement
+		return switch(month) {
+			case FEBRUARY, MARCH, APRIL 		-> "Spring";
+			case MAY, JUNE, JULY 				-> "Summer";
+			case SEPTEMBER, OCTOBER 			-> "Autumn";
+			case NOVEMBER, DECEMBER, JANUARY 	-> "Winter";
+			
+		}; //; required after curly brackets that contain an expression
 	}
 	
 }
