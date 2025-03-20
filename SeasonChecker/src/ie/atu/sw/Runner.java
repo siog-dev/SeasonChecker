1a2d1470408e1228ffcd24a9ad9cf6f214495bd5
siog <siog@LAPTOP-545TJ6PE>
2025-03-20 12:44:46 +0000

    call getSeason() method with a date argument and print the return value

--- SeasonChecker/src/ie/atu/sw/Runner.java
+++ SeasonChecker/src/ie/atu/sw/Runner.java
@@ -1,9 +1,25 @@
 package ie.atu.sw;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Runner {
 	
 	public static void main(String[] args) {
 		var sc = new SeasonChecker();
+		/*
+		 * use ctrl + space to import LocalDate when typing
+		 * 
+		 * use dot notation and "of",
+		 * select option with "Month" (rather than int for month)
+		 * 
+		 * add year and day in decimals
+		 * use ctrl + space while typing month(eg aug)
+		 * select AUGUS to import java.time.Month
+		 */
+		String season = sc.getSeason(LocalDate.of(1999, Month.AUGUST, 20));
+		//LocalDate = one argument comprising of three values (year, month, day)
+		System.out.println(season);
 	}
 	
 }
